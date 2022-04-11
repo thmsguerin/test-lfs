e756ed2bf2f9e2971c036dc46970372ac28dfe00
Mathieu-Vialat <mathieu.vialat@le-campus-numerique.fr>
2022-04-11 13:54:35 +0200

    Fix the sorting (SortRatingsField_fixed)

--- DataFrame_Groupby.py
+++ DataFrame_Groupby.py
@@ -60,7 +60,7 @@ print('\n==================================================================\n')
 
 
 # Sort data ratings by created field (groupby + lambda function + sorted)
-sortRatingsField = cloneDF(mergeRatings)
-sortRatingsField = sortRatingsField.groupby(['movie_id', 'title'])['rating'].agg(
-    COUNT=np.size, myAVG=lambda x: x.sum() / float(x.count())).sort('COUNT', ascending=False)
+sortRatingsField_fixed = cloneDF(mergeRatings)
+sortRatingsField_fixed = sortRatingsField.groupby(['movie_id', 'title'])['rating'].agg(
+    COUNT=np.size, myAVG=lambda x: x.sum() / float(x.count())).sort_values('COUNT', ascending=False)
 print('My info sorted: \n%s' % sortRatingsField[:15])
